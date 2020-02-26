2841c56cb837803d1ebb66079efeba2f913cc193
Andrew Reid <areid.sj@gmail.com>
2020-02-26 10:10:09 -0700

    Write and pass initial vendor tests

---
+++ test/vendor_test.rb
@@ -0,0 +1,20 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/vendor'
+
+class VendorTest < Minitest::Test
+
+  def setup
+    @vendor = Vendor.new("Rocky Mountain Fresh")
+  end
+
+  def test_it_exists
+    assert_instance_of Vendor, @vendor
+  end
+
+  def test_it_has_attributes
+    assert_equal "Rocky Mountain Fresh",  @vendor.name
+    assert_equal ({}), @vendor.inventory
+  end
+
+end
